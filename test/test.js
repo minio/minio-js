7bb8d531740a2b7cf4a07218ac24ed5ab312206a
Frederick F. Kautz IV <fkautz@minio.io>
2015-05-24 16:34:09 -0700

    Adding error parsing

--- test/test.js
+++ test/test.js
@@ -8,16 +8,22 @@ var minio = require('../..');
 describe ('Client', function() {
     "use strict";
     var client = minio.getClient({address: 'localhost:9000'})
-    describe ('#createBucket(bucket, callback)', function() {
-        it('should call the callback on success', function(done) {
-            nock('http://localhost:8080').put('/bucket').reply(200)
-            client.createBucket('bucket', done)
-        })
-        it('pass an error into the callback on failure', function(done) {
-            nock('http://localhost:8080').put('/bucket').reply(400)
-            client.createBucket('bucket', checkError(done))
+    describe("service level", function() {
+        describe ('#createBucket(bucket, callback)', function() {
+            it('should call the callback on success', function(done) {
+                nock('http://localhost:8080').put('/bucket').reply(200)
+                client.createBucket('bucket', done)
+            })
+            it('pass an error into the callback on failure', function(done) {
+                nock('http://localhost:8080').put('/bucket').reply(400, "<Error><Status>status</Status><Message>message</Message><RequestId>resourceid</RequestId><Resource>/bucket</Resource></Error>")
+                client.createBucket('bucket', checkError(done))
+            })
         })
     })
+    describe("bucket level", function() { })
+    describe("object level", function() { })
+    describe("helpers", function() { })
+    describe("internal helpers", function() { })
 })
 
 var checkError = function(done) {
