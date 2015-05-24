980121049737768567a6c3adc2bc490269aac373
Frederick F. Kautz IV <fkautz@minio.io>
2015-05-24 11:31:34 -0700

    Initial layout and test infrastructure

--- test/test.js
+++ test/test.js
@@ -1,18 +1,20 @@
 require('source-map-support').install();
 
 var assert = require('assert');
+var nock = require('nock')
+
 var minio = require('../..');
 
-describe('Object Storage Client', function () {
-        "use strict";
-        describe("#getClient()", function () {
-            it('should return a client', function () {
-                var client = minio.getClient();
-                assert.equal('hello', client);
-            })
-            it('should return index when the value is not present', function () {
-                assert.equal(0, [2, 3, 4].indexOf(2))
-            })
+describe ('Client', function() {
+    "use strict";
+    var client = minio.getClient({address: 'localhost:9000'})
+    client.setTransport(nock)
+    describe ('#createBucket(bucket, callback)', function() {
+        it('should call the callback on success', function(done) {
+            client.createBucket('bucket', done)
         })
-    }
-)
+        it('should call the callback on success', function(done) {
+            client.createBucket('bucket', done)
+        })
+    })
+})
