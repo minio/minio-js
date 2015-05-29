f1027ecf2fa02759fcc840458d43479f7cbff8fd
Frederick F. Kautz IV <fkautz@minio.io>
2015-05-28 20:04:44 -0700

    Adding new transport

---
+++ src/test/transport.js
@@ -0,0 +1,85 @@
+/*
+ * Minimal Object Storage Library, (C) 2015 Minio, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+var Stream = require('stream')
+
+class MockTransport {
+    constructor() {
+        "use strict"
+        this.requests = []
+    }
+
+    addRequest(verifyParams, statusCode, responseHeaders, responseStream) {
+        "use strict"
+        var req = {
+            verifyParams: verifyParams,
+            statusCode: statusCode,
+            responseHeaders: responseHeaders,
+            responseStream: responseStream
+        }
+        this.requests.push(req)
+    }
+    clearRequests() {
+        "use strict"
+        this.requests = []
+    }
+    request(params, callback) {
+        "use strict"
+        var req = this.requests.shift()
+        return new Request(req, params, callback)
+    }
+}
+
+class Request {
+    constructor(req, params, callback) {
+        "use strict";
+        this.req = req
+        this.params = params
+        this.callback = callback
+    }
+    end() {
+        "use strict";
+        this._r()
+    }
+    _r() {
+        "use strict";
+        var stream = new Stream.Readable()
+        stream._read = () => {
+        }
+        if (this.req.responseStream) {
+            this.req.responseStream.pipe(stream)
+        } else {
+            stream.push(null)
+        }
+        if (this.req.verifyParams) {
+            this.req.verifyParams(this.params)
+        }
+        if (this.req.statusCode) {
+            stream.statusCode = this.req.statusCode
+        } else {
+            stream.statusCode = 200
+        }
+        if (this.req.responseHeaders) {
+            stream.headers = this.req.responseHeaders
+        } else {
+            stream.headers = {}
+        }
+        this.callback(stream)
+    }
+}
+
+var inst = MockTransport
+module.exports = inst
