a3f7fa225291f0b5849b4503a5a090b42eccefe1
Frederick F. Kautz IV <fkautz@minio.io>
2015-06-05 18:23:44 -0700

    Adding get object example

---
+++ src/example/get-object.js
@@ -0,0 +1,39 @@
+/*
+ * Minimal Object Storage Library, (C) 2015 Minio, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+var Minio = require('../..')
+var Through2 = require('through2')
+
+var s3client = new Minio({
+  host: 's3.amazonaws.com',
+  port: 80,
+  accessKey: 'YOUR-ACCESSKEYID',
+  secretKey: 'YOUR-SECRETACCESSKEY'
+})
+
+var size = 0
+s3client.getObject('goroutine', 'hello/11mb', function(e, dataStream) {
+  if(e) {
+    return console.log(e)
+  }
+  dataStream.pipe(Through2(function(chunk, enc, done) {
+    size += chunk.length
+    done()
+  }, function(done) {
+    console.log('total size: ' + size)
+    done()
+  }))
+})
