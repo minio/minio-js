2a476fef57fdca0ab95e77482ca8cdd2a5677128
koolhead17 <koolhead17@gmail.com>
2016-08-14 15:23:01 -0700

    Code:s3-endpoints.js/Added ap-south-1 region to the list of available s3 regions.

--- src/main/s3-endpoints.js
+++ src/main/s3-endpoints.js
@@ -24,6 +24,7 @@ let awsS3Endpoint = {
   'eu-west-1': 's3-eu-west-1.amazonaws.com',
   'sa-east-1': 's3-sa-east-1.amazonaws.com',
   'eu-central-1': 's3-eu-central-1.amazonaws.com',
+  'ap-south-1': 's3-ap-south-1.amazonaws.com',
   'ap-southeast-1': 's3-ap-southeast-1.amazonaws.com',
   'ap-southeast-2': 's3-ap-southeast-2.amazonaws.com',
   'ap-northeast-1': 's3-ap-northeast-1.amazonaws.com'
