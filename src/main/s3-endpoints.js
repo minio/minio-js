fef62f34b27b092eee494fc9257e857e5ee31895
koolhead17 <koolhead17@gmail.com>
2016-12-16 12:39:16 +0530

    s3endpoints:Added eu-west-2 region to the list.

--- src/main/s3-endpoints.js
+++ src/main/s3-endpoints.js
@@ -23,6 +23,7 @@ let awsS3Endpoint = {
   'us-west-1': 's3-us-west-1.amazonaws.com',
   'us-west-2': 's3-us-west-2.amazonaws.com',
   'eu-west-1': 's3-eu-west-1.amazonaws.com',
+  'eu-west-2': 's3-eu-west-2.amazonaws.com',
   'sa-east-1': 's3-sa-east-1.amazonaws.com',
   'eu-central-1': 's3-eu-central-1.amazonaws.com',
   'ap-south-1': 's3-ap-south-1.amazonaws.com',
