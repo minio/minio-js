244e621cf2b7bc1f8aaddf310eaa35581ffe60bd
Brendan Ashworth <brendan.ashworth@me.com>
2017-07-06 12:38:37 -0700

    add promisify helper to helpers
    This function will wrap a callback function and replace it with one that
    accepts both callbacks and returns promises.

--- src/main/helpers.js
+++ src/main/helpers.js
@@ -17,6 +17,34 @@
 import stream from 'stream'
 import mime from 'mime-types'
 
+// Returns a wrapper function that will promisify a given callback function.
+// It will preserve 'this'.
+export function promisify(fn) {
+  return function() {
+    // If the last argument is a function, assume its the callback.
+    let callback = arguments[arguments.length - 1]
+
+    // If the callback is given, don't promisify, just pass straight in.
+    if (typeof callback === 'function') return fn.apply(this, arguments)
+
+    // Otherwise, create a new set of arguments, and wrap
+    // it in a promise.
+    let args = [...arguments]
+
+    return new Promise((resolve, reject) => {
+      // Add the callback function.
+      args.push((err, value) => {
+        if (err) return reject(err)
+
+        resolve(value)
+      })
+
+      // Call the function with our special adaptor callback added.
+      fn.apply(this, args)
+    })
+  }
+}
+
 // All characters in string which are NOT unreserved should be percent encoded.
 // Unreserved characers are : ALPHA / DIGIT / "-" / "." / "_" / "~"
 // Reference https://tools.ietf.org/html/rfc3986#section-2.2
