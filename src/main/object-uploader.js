eead7ee08b38d03961ce73495426c044ff9abe59
shruthied <32338806+shruthied@users.noreply.github.com>
2018-01-04 09:38:38 -0600

    Update object-uploader.js

--- src/main/object-uploader.js
+++ src/main/object-uploader.js
@@ -22,7 +22,7 @@ import * as querystring from 'querystring'
 export default class ObjectUploader extends Transform {
   constructor(client, bucketName, objectName, partSize, contentType, callback) {
     super()
-    this.emptyStream=true;
+    this.emptyStream=true
     this.client = client
     this.bucketName = bucketName
     this.objectName = objectName
@@ -58,7 +58,7 @@ export default class ObjectUploader extends Transform {
   }
 
   _transform(chunk, encoding, callback) {
-    this.emptyStream=false;
+    this.emptyStream=false
     let method = 'PUT'
     let headers = {
       'Content-Length': chunk.length,
@@ -208,12 +208,12 @@ export default class ObjectUploader extends Transform {
   }
 
   _flush(callback) {
-   if (this.emptyStream) {
-     let method = 'PUT'
-     let headers = {
-       'Content-Length': 0,
-       'Content-Type': this.contentType
-     }
+     if (this.emptyStream) {
+       let method = 'PUT'
+       let headers = {
+        'Content-Length': 0,
+        'Content-Type': this.contentType
+       }
      let options = {
        method, headers,
        query: '',
@@ -263,6 +263,6 @@ export default class ObjectUploader extends Transform {
 
                                           callback()
                                         })
+     }
   }
-}
 }
