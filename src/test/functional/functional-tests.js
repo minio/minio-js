3cb9caf3bee3a87e77a8cd841d2cebbd124cfcac
Joao Santos <j.santos@whisbi.com>
2022-07-29 14:17:47 +0200

    fix: add more unit test for content disposition

--- src/test/functional/functional-tests.js
+++ src/test/functional/functional-tests.js
@@ -1101,12 +1101,12 @@ describe('functional tests', function () {
       })
     })
 
-    step('presignedPostPolicy(postPolicy, cb)_postPolicy:setContentDisposition_', done => {
+    step('presignedPostPolicy(postPolicy, cb)_postPolicy:setContentDisposition_inline', done => {
       var policy = client.newPostPolicy()
       var objectName = 'test-content-disposition' + uuid.v4()
       policy.setKey(objectName)
       policy.setBucket(bucketName)
-      policy.setContentDisposition('fake-text')
+      policy.setContentDisposition('inline')
 
       client.presignedPostPolicy(policy, (e, data) => {
         if (e) return done(e)
@@ -1114,6 +1114,26 @@ describe('functional tests', function () {
         _.each(data.formData, (value, key) => req.field(key, value))
         req.attach('file', Buffer.from([_1byte]), 'test')
         req.end(function(e) {
+          if (e) return done(e)
+          client.removeObject(bucketName, objectName, done)
+        })
+        req.on('error', e => done(e))
+      })
+    })
+
+    step('presignedPostPolicy(postPolicy, cb)_postPolicy:setContentDisposition_attachment', done => {
+      var policy = client.newPostPolicy()
+      var objectName = 'test-content-disposition' + uuid.v4()
+      policy.setKey(objectName)
+      policy.setBucket(bucketName)
+      policy.setContentDisposition('attachment; filename=  My* Docume!  nt.json')
+
+      client.presignedPostPolicy(policy, (e, data) => {
+        if (e) return done(e)
+        var req = superagent.post(data.postURL)
+        _.each(data.formData, (value, key) => req.field(key, value))
+        req.attach('file', Buffer.from([_1byte]), 'test')
+        req.end(function (e) {
           if (e) return done(e)
           client.removeObject(bucketName, objectName, done)
         })
