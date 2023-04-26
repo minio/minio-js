832ac71a6be9b088c9f8e65226af544fdc8c764f
Anton Skshidlevsky <meefik@gmail.com>
2023-04-27 01:34:49 +0200

    Fixed issue with closing files in fPutObject() function (#1112)

--- src/main/minio.js
+++ src/main/minio.js
@@ -1034,6 +1034,7 @@ export class Client {
       cb => fs.stat(filePath, cb),
       (stats, cb) => {
         size = stats.size
+        var stream
         var cbTriggered = false
         var origCb = cb
         cb = function () {
@@ -1041,6 +1042,7 @@ export class Client {
             return
           }
           cbTriggered = true
+          if (stream) stream.destroy()
           return origCb.apply(this, arguments)
         }
         if (size > this.maxObjectSize) {
@@ -1060,7 +1062,7 @@ export class Client {
             .on('data', data => {
               var md5sum = data.md5sum
               var sha256sum = data.sha256sum
-              var stream = fs.createReadStream(filePath, options)
+              stream = fs.createReadStream(filePath, options)
               uploader(stream, size, sha256sum, md5sum, (err, objInfo) => {
                 callback(err, objInfo)
                 cb(true)
@@ -1095,6 +1097,7 @@ export class Client {
         async.whilst(
           cb => { cb(null, uploadedSize < size) },
           cb => {
+            var stream
             var cbTriggered = false
             var origCb = cb
             cb = function () {
@@ -1102,6 +1105,7 @@ export class Client {
                 return
               }
               cbTriggered = true
+              if (stream) stream.destroy()
               return origCb.apply(this, arguments)
             }
             var part = parts[partNumber]
@@ -1126,7 +1130,7 @@ export class Client {
                   return cb()
                 }
                 // part is not uploaded yet, or md5 mismatch
-                var stream = fs.createReadStream(filePath, options)
+                stream = fs.createReadStream(filePath, options)
                 uploader(uploadId, partNumber, stream, length,
                          data.sha256sum, data.md5sum, (e, objInfo) => {
                            if (e) return cb(e)
