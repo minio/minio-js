5a3eaeb9b3c969e97f5ec0457c41c4d276b586ab
Trim21 <trim21.me@gmail.com>
2023-05-12 17:05:09 +0800

    remove unused

--- src/internal/async.ts
+++
@@ -1,9 +0,0 @@
-// promise helper for stdlib
-
-import * as stream from 'node:stream'
-import { promisify } from 'node:util'
-
-export const streamPromise = {
-  // node:stream/promises Added in: v15.0.0
-  pipeline: promisify(stream.pipeline),
-}
