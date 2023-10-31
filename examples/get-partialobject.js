a89b4ae653d55e8f91a545b983386f2d1f28b039
Trim21 <trim21.me@gmail.com>
2023-10-31 20:59:53 +0800

    Update get-partialobject.js

--- examples/get-partialobject.js
+++ examples/get-partialobject.js
@@ -17,44 +17,36 @@
 // Note: YOUR-ACCESSKEYID, YOUR-SECRETACCESSKEY, my-bucketname and my-objectname
 // are dummy values, please replace them with original values.
 
-var Minio = require('minio')
+import * as Minio from 'minio'
 
-var s3Client = new Minio.Client({
+const s3Client = new Minio.Client({
   endPoint: 's3.amazonaws.com',
   accessKey: 'YOUR-ACCESSKEYID',
   secretKey: 'YOUR-SECRETACCESSKEY',
 })
 
 // Download the object my-objectname at an offset 1024, for a total of 4096 bytes.
-var size = 0
-s3Client.getPartialObject('my-bucketname', 'my-objectname', 1024, 4096, function (e, dataStream) {
-  if (e) {
-    return console.log(e)
-  }
-  dataStream.on('data', function (chunk) {
-    size += chunk.length
-  })
-  dataStream.on('end', function () {
-    console.log('End. Total size = ' + size)
-  })
-  dataStream.on('error', function (e) {
-    console.log(e)
-  })
+let size = 0
+const dataStrdam = await s3Client.getPartialObject('my-bucketname', 'my-objectname', 1024, 4096)
+dataStream.on('data', function (chunk) {
+  size += chunk.length
+})
+dataStream.on('end', function () {
+  console.log('End. Total size = ' + size)
+})
+dataStream.on('error', function (e) {
+  console.log(e)
 })
 
-var versionedObjSize = 0
+let versionedObjSize = 0
 // reads 30 bytes from the offset 10.
-s3Client.getPartialObject('mybucket', 'photo.jpg', 10, 30, { versionId: 'my-versionId' }, function (err, dataStream) {
-  if (err) {
-    return console.log(err)
-  }
-  dataStream.on('data', function (chunk) {
-    versionedObjSize += chunk.length
-  })
-  dataStream.on('end', function () {
-    console.log('End. Total size = ' + versionedObjSize)
-  })
-  dataStream.on('error', function (err) {
-    console.log(err)
-  })
+const dataStream2 = await s3Client.getPartialObject('mybucket', 'photo.jpg', 10, 30, { versionId: 'my-versionId' })
+dataStream2.on('data', function (chunk) {
+  versionedObjSize += chunk.length
+})
+dataStream2.on('end', function () {
+  console.log('End. Total size = ' + versionedObjSize)
+})
+dataStream2.on('error', function (err) {
+  console.log(err)
 })
