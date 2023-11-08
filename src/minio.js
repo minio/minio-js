3273e7b9fca73c44cdaf3c8a52ba3d83a449f2d8
Trim21 <trim21.me@gmail.com>
2023-11-08 23:49:30 +0800

    remove dependency `xml` (#1231)

--- src/minio.js
+++ src/minio.js
@@ -23,7 +23,6 @@ import BlockStream2 from 'block-stream2'
 import _ from 'lodash'
 import * as querystring from 'query-string'
 import { TextEncoder } from 'web-encoding'
-import Xml from 'xml'
 import xml2js from 'xml2js'
 
 import * as errors from './errors.ts'
@@ -169,19 +168,13 @@ export class Client extends TypedClient {
     // sending makeBucket request with XML containing 'us-east-1' fails. For
     // default region server expects the request without body
     if (region && region !== DEFAULT_REGION) {
-      var createBucketConfiguration = []
-      createBucketConfiguration.push({
-        _attr: {
-          xmlns: 'http://s3.amazonaws.com/doc/2006-03-01/',
+      const builder = new xml2js.Builder()
+      payload = builder.buildObject({
+        CreateBucketConfiguration: {
+          $: { xmlns: 'http://s3.amazonaws.com/doc/2006-03-01/' },
+          LocationConstraint: region,
         },
       })
-      createBucketConfiguration.push({
-        LocationConstraint: region,
-      })
-      var payloadObject = {
-        CreateBucketConfiguration: createBucketConfiguration,
-      }
-      payload = Xml(payloadObject)
     }
     var method = 'PUT'
     var headers = {}
@@ -1406,23 +1399,20 @@ export class Client extends TypedClient {
     var method = 'POST'
     var query = `uploadId=${uriEscape(uploadId)}`
 
-    var parts = []
-
-    etags.forEach((element) => {
-      parts.push({
-        Part: [
-          {
-            PartNumber: element.part,
-          },
-          {
-            ETag: element.etag,
-          },
-        ],
-      })
+    const builder = new xml2js.Builder()
+    const payload = builder.buildObject({
+      CompleteMultipartUpload: {
+        $: {
+          xmlns: 'http://s3.amazonaws.com/doc/2006-03-01/',
+        },
+        Part: etags.map((etag) => {
+          return {
+            PartNumber: etag.part,
+            ETag: etag.etag,
+          }
+        }),
+      },
     })
-
-    var payloadObject = { CompleteMultipartUpload: parts }
-    var payload = Xml(payloadObject)
 
     this.makeRequest({ method, bucketName, objectName, query }, payload, [200], '', true, (e, response) => {
       if (e) {
