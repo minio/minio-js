cf3a1b68746123d9f399edc941ed56a8461c72c1
Trim21 <trim21.me@gmail.com>
2023-11-09 23:03:17 +0800

    Fix presigned get object request date

--- examples/presigned-getobject-request-date.js
+++ examples/presigned-getobject-request-date.js
@@ -28,7 +28,6 @@ const s3Client = new Minio.Client({
 
 // Presigned get object URL for my-objectname at my-bucketname, it expires in 7 days by default.
 const requestDate = new Date()
-requestDate.setHours(0, 0, 0, 0)
 
 requestDate.setHours(0, 0, 0, 0)
 s3Client.presignedGetObject('my-bucketname', 'my-objectname', 1000, {}, requestDate, function (e, presignedUrl) {
