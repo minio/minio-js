264819ff969eef6c2a5dfa4fc15f588330cf26b5
Reinaldy Rafli <aldy505@proton.me>
2023-11-24 11:09:20 +0700

    test: join-host-port

--- src/internal/join-host-port.ts
+++ src/internal/join-host-port.ts
@@ -2,22 +2,22 @@
  * joinHostPort combines host and port into a network address of the
  * form "host:port". If host contains a colon, as found in literal
  * IPv6 addresses, then JoinHostPort returns "[host]:port".
- * 
- * @param host 
- * @param port 
+ *
+ * @param host
+ * @param port
  * @returns Cleaned up host
  * @internal
  */
 export function joinHostPort(host: string, port?: number): string {
-    if (port === undefined) {
-        return host
-    }
-    
-	// We assume that host is a literal IPv6 address if host has
-	// colons.
-    if (host.includes(":")) {
-        return `[${host}]:${port.toString()}`
-    }
+  if (port === undefined) {
+    return host
+  }
 
-    return `${host}:${port.toString()}`
+  // We assume that host is a literal IPv6 address if host has
+  // colons.
+  if (host.includes(':')) {
+    return `[${host}]:${port.toString()}`
+  }
+
+  return `${host}:${port.toString()}`
 }
